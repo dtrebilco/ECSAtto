c0d7b75d1394214c5b44c85fe8c573c5cd2dde23
Damian <dtrebilco@gmail.com>
2018-06-09 23:54:15 +1000

    ECS work on manager

--- ECS/EntityManager.cpp
+++ ECS/EntityManager.cpp
@@ -9,14 +9,16 @@ EntityManager::EntityID EntityManager::CreateEntity()
   // Check if there is a deleted entity to reuse
   if (m_deletedEntityOffset < m_entities.size())
   {
-    retID = m_deletedEntityOffset;
+    retID = (EntityID)m_deletedEntityOffset;
+    
     // Move to next in the list
-    m_deletedEntityOffset = m_entities[retID].m_offset;
-    m_entities[retID] = newEntity;
+    Entity& entity = m_entities[m_deletedEntityOffset];
+    m_deletedEntityOffset = entity.m_offset;
+    entity = newEntity;
   }
   else
   {
-    retID = (uint32_t)m_entities.size();
+    retID = (EntityID)m_entities.size();
     m_entities.push_back(newEntity);
   }
   return retID;
@@ -24,17 +26,65 @@ EntityManager::EntityID EntityManager::CreateEntity()
 
 void EntityManager::DeleteEntity(EntityID i_entity)
 {
-  AT_ASSERT(i_entity < m_entities.size());
-
   // Null out all Component IDs
+  Entity& entity = m_entities[(uint16_t)i_entity];
+  uint32_t count = PopCount64(entity.m_componentMask);
+  for (uint32_t i = 0; i < count; i++)
+  {
+    m_entityComponents[entity.m_offset + i] = INVALID_CID;
+  }
 
   // Set lowest null ID counter
+  // DT_TODO:
 
   // Set offset to the ID
-  m_entities[i_entity].m_componentMask = 0;
-  m_entities[i_entity].m_flags = 0;
-  m_entities[i_entity].m_offset = m_deletedEntityOffset;
-  m_deletedEntityOffset = i_entity;
+  entity.m_componentMask = 0;
+  entity.m_flags = 0;
+  entity.m_offset = m_deletedEntityOffset;
+  m_deletedEntityOffset = (uint32_t)i_entity;
 }
 
+void EntityManager::Update()
+{
+  // If lowest NULL id is less than the limit, update
+}
+
+void EntityManager::AddComponent(EntityID i_entity, ComponentType i_component, ComponentID i_id)
+{
+  AT_ASSERT(!HasComponent(i_entity, i_component));
+
+  // If the entity is at the end of the array, insert in place
+
+
+  // Else, copy to the end of the array first
+
+  // Set the new ID value
+
+}
+
+void EntityManager::RemoveComponent(EntityID i_entity, ComponentType i_component)
+{
+  AT_ASSERT(HasComponent(i_entity, i_component));
+
+  // Unset flag
+
+  // Move components down
+
+  // Set slot to invalid
+    // Update invalid slot index
+}
+
+void EntityManager::SetComponentID(EntityID i_entity, ComponentType i_component, ComponentID i_id)
+{
+  AT_ASSERT(HasComponent(i_entity, i_component));
+
+  uint32_t offset = GetComponentOffset(i_entity, i_component);
+  m_entityComponents[offset] = i_id;
+}
+
+EntityManager::ComponentID EntityManager::GetComponentID(EntityID i_entity, ComponentType i_component) const
+{
+  uint32_t offset = GetComponentOffset(i_entity, i_component);
+  return m_entityComponents[offset];
+}
 
--- ECS/EntityManager.h
+++ ECS/EntityManager.h
@@ -1,37 +1,54 @@
 #pragma once
+#include "Common.h"
 
 #include <cstdint>
 #include <vector>
 
-#include "Common.h"
-
+/// \brief This class manages entities in the system. 
+/// It is expected that it will be overridden with a game specific version that does custom behavior on adding and removing entities.
+/// This class is optimized with the expected usage of adding components to newly created entities. 
+/// Not creating lots of entities then adding components to them later.
+///   ie.  Do: CreateEntity(), AddComponent(), AddComponent() Then, CreateEntity(), AddComponent(), AddComponent()
+///        Rather than: CreateEntity(), CreateEntity(),  Then  AddComponent(), AddComponent(), AddComponent(), AddComponent()
 class EntityManager
 {
 public:
 
-  typedef uint16_t EntityID;      //!< Support 65k entities
-  typedef uint16_t ComponentID;   //!< Support 65k entries per component type
-  typedef uint64_t ComponentMask; //!< Support 64 components per entity
+  enum class EntityID : uint16_t {};    //!< Support 65k entities
+  enum class ComponentID : uint16_t {}; //!< Support 65k entries per component type
+  typedef uint64_t ComponentMask;       //!< Support 64 components per entity (could use std::bitset?)
 
+  const EntityID INVALID_EID = (EntityID)UINT16_MAX;
+  const ComponentID INVALID_CID = (ComponentID)UINT16_MAX;
+
+  /// \brief Create an entity.
   EntityID CreateEntity();
+
+  /// \brief Delete an entity.
+  /// \param i_entity The entity to delete.
   void DeleteEntity(EntityID i_entity);
 
+  /// \brief Test if a entity has a component on it
+  /// \param i_entity The entity to test
+  /// \param i_component The component to test for
+  /// \return Returns true if the component exists on the entity
   inline bool HasComponent(EntityID i_entity, ComponentType i_component) const
   {
-    return m_entities[i_entity].m_componentMask & GetMask(i_component);
+    return m_entities[(uint16_t)i_entity].m_componentMask & GetMask(i_component);
   }
 
-  void Update(); // Cleanup and maintenance of data
+  /// \brief Update the manager - cleans up and organizes data
+  void Update();
 
 protected:
   friend class ComponentManager;
 
   // Component Manager Interface
-  bool AddComponent(EntityID i_entity, ComponentType i_component, ComponentID i_id);
-  bool RemoveComponent(EntityID i_entity, ComponentType i_component);
+  void AddComponent(EntityID i_entity, ComponentType i_component, ComponentID i_id);
+  void RemoveComponent(EntityID i_entity, ComponentType i_component);
 
-  bool SetComponentID(EntityID i_entity, ComponentType i_component, ComponentID i_id);
-  ComponentID GetComponentID(EntityID i_entity, ComponentType i_component);
+  void SetComponentID(EntityID i_entity, ComponentType i_component, ComponentID i_id);
+  ComponentID GetComponentID(EntityID i_entity, ComponentType i_component) const;
 
 private:
 
@@ -40,15 +57,24 @@ private:
     return (ComponentMask)1 << (uint32_t)i_component;
   }
 
+  /// \brief Get the offset into the components data array
+  inline uint32_t GetComponentOffset(EntityID i_entity, ComponentType i_component) const
+  {
+    const Entity& entity = m_entities[(uint16_t)i_entity];
+    return entity.m_offset + PopCount64(entity.m_componentMask & (GetMask(i_component) - 1));
+  }
+
   struct Entity
   {
     ComponentMask m_componentMask; //!< Mask indicating what components are used
-    uint32_t m_offset; //!< Offset for the components
-    uint32_t m_flags;  //!< Custom flags per entity
+    uint32_t m_offset;             //!< Offset for the components
+    uint32_t m_flags;              //!< Custom flags per entity
   };
+  static_assert(sizeof(Entity) == 16, "Unexpected entity size");
 
-  std::vector<Entity> m_entities; //!< Array of entities - may have unused entities
+  std::vector<Entity> m_entities;              //!< Array of entities - may have unused entities
   std::vector<ComponentID> m_entityComponents; //!< Array of components per entity
 
   uint32_t m_deletedEntityOffset = UINT32_MAX; //!< The first deleted entity (if any)
 };
+
