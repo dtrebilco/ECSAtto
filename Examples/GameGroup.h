61f53aa400405cb36a51a2a64702e4a2903ef24f
Damian <dtrebilco@gmail.com>
2018-11-05 21:56:44 +1000

    Clean unused data

--- Examples/GameGroup.h
+++ Examples/GameGroup.h
@@ -8,8 +8,6 @@ class WorldTransforms;
 class Bounds;
 class WorldBounds;
 
-class FlagTest : public FlagManager {};
-
 class GameGroup : public EntityGroup
 {
 public:
@@ -22,12 +20,10 @@ public:
 
   std::unique_ptr<Bounds> m_bounds;
   std::unique_ptr<WorldBounds> m_worldBounds;
-  std::unique_ptr<FlagTest> m_flagTest;
 };
 
 template<> inline Transforms& GetManager<Transforms>(GameGroup& i_group) { return *i_group.m_transforms; }
 template<> inline WorldTransforms& GetManager<WorldTransforms>(GameGroup& i_group) { return *i_group.m_worldTransforms; }
 template<> inline Bounds& GetManager<Bounds>(GameGroup& i_group) { return *i_group.m_bounds; }
 template<> inline WorldBounds& GetManager<WorldBounds>(GameGroup& i_group) { return *i_group.m_worldBounds; }
-template<> inline FlagTest& GetManager<FlagTest>(GameGroup& i_group) { return *i_group.m_flagTest; }
 
